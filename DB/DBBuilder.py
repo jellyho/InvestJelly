59abb0c5cdf7f2d3fb1fde57b4fa5b98d4241804
jellyho <jellyho@naver.com>
2021-11-12 23:08:15 +0900

    Update DBBuilder.py

--- DB/DBBuilder.py
+++ DB/DBBuilder.py
@@ -29,29 +29,30 @@ class BithumbDBUpdater(Mysql):
     빗썸 Public API를 이용하는 pybithumb 패키지를 이용하여 주기적으로 암호화폐의 거래정보를 다운로드, mysql 데이터베이스에 저장.
     """
     self.interval = ['1m', '3m', '5m', '10m', '30m', '1h', '6h', '12h', '24h' ]
-      #DB에 데이터베이스 생성(첫 실행시)
-    with self.__conn.cursor() as curs:
-      for d in self.interval:
-          sql = f"""
-          create table if not exists bithumb_{d}_ohlcv (
-          code VARCHAR(20),
-          date TIMESTAMP,
-          open DOUBLE(20, 4),
-          high DOUBLE(20, 4),
-          low DOUBLE(20, 4),
-          close DOUBLE(20, 4),
-          volume DOUBLE(20, 4),
-          PRIMARY KEY (code, date)
-          )
-          """
-          curs.execute(sql)
-      self.__conn.commit()
+    #DB에 데이터베이스 생성(첫 실행시)
+    def __init__(self, host, user, password, db):
+        self.__init__.super(self, Mysql)
+        with self.__conn.cursor() as curs:
+            for d in self.interval:
+              sql = f"""
+              create table if not exists bithumb_{d}_ohlcv (
+              code VARCHAR(20),
+              date TIMESTAMP,
+              open DOUBLE(20, 4),
+              high DOUBLE(20, 4),
+              low DOUBLE(20, 4),
+              close DOUBLE(20, 4),
+              volume DOUBLE(20, 4),
+              PRIMARY KEY (code, date)
+              )
+              """
+              curs.execute(sql)
+            self.__conn.commit()
  
     def Update(self):
 		#업데이트 시작
-		self.__connectDB();
+        self.__connectDB()
 		print("Update Start", datetime.today())
-
 		#업데이트시 오류 생겨도 프로그램 종료 방지
 		with self.__conn.cursor() as curs:
 			for d in self.interval:
@@ -70,24 +71,24 @@ class BithumbDBUpdater(Mysql):
 							  if df is None:
 								  continue
 							  #업데이트할 데이터만 남기고 제거.
-							  if not rs[0] is None:
-								  df = df[rs[0]:]
-
-							  #가장 최근 데이터는 시간대가 정확하지 않으므로 제거.
-							  df = df.drop(today)
-
-							  #업데이트 현황 프린트
-							  print('\r'+f'Adding {len(df)} rows of {d}_{t}', end=" - ") 
-
-							  #DB업데이트 쿼리문
-							  sql = f"REPLACE INTO bithumb_{d}_ohlcv (code, date, open, high, low, close, volume) VALUES "
-
-							  for r in df.itertuples():
-								sql += f"('{t}', '{r.Index}', {r.open}, {r.high}, {r.low}, {r.close}, {r.volume}), "
-							  sql = sql[:-2]
-							  curs.execute(sql)
-							  self.__conn.commit()
-							  print('finished')
+                            if not rs[0] is None:
+                              df = df[rs[0]:]
+                            
+                            #가장 최근 데이터는 시간대가 정확하지 않으므로 제거.
+                            df = df.drop(today)
+                            
+                            #업데이트 현황 프린트
+                            print('\r'+f'Adding {len(df)} rows of {d}_{t}', end=" - ") 
+                            
+                            #DB업데이트 쿼리문
+                            sql = f"REPLACE INTO bithumb_{d}_ohlcv (code, date, open, high, low, close, volume) VALUES "
+                            
+                            for r in df.itertuples():
+                                sql += f"('{t}', '{r.Index}', {r.open}, {r.high}, {r.low}, {r.close}, {r.volume}), "
+                            sql = sql[:-2]
+                            curs.execute(sql)
+                            self.__conn.commit()
+                            print('finished')
 						except Exception as e:
 							print(f'Network Error')
 				except Exception as e:
