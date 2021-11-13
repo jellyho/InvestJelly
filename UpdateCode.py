c4462392491da7bf33fd68a7f2e2276fba845409
jellyho <jellyho@naver.com>
2021-11-13 18:03:07 +0900

    Update UpdateCode.py

--- UpdateCode.py
+++ UpdateCode.py
@@ -1,5 +1,5 @@
-from InvestJelly.DB import Updater as u
-from InvestJelly.DB import Bithumb
+from .DB import Updater as u
+from .DB import Bithumb
 updater = u('jellyho.cyeickq2ncom.ap-northeast-2.rds.amazonaws.com', 'admin', 'qlalfqjsgh1!', 'crypto')
 updater.add(Bithumb.market_krw('all'))
 updater.update_Timer(4)
