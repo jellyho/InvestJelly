431459e18d4ef04c2fec1531419c4156562a0ee6
jellyho <jellyho@naver.com>
2021-11-14 23:36:15 +0900

    Update Bithumb.py

--- DB/Readers/Bithumb.py
+++ DB/Readers/Bithumb.py
@@ -19,22 +19,31 @@ class tickers_krw:
     return df
 
 class ohlcv_krw:
-    """
-    기간 입력 안되었을 때 최소 일자, 최대 일자로 초기화 됨
-    code: 불러올 코인의 코드
-    interval: 불러올 거래 데이터 간격
-    start: 불러올 데이터의 시작 날짜 %Y-%m-%d %H:%M:%S
-    """
-    self._connectDB()
+  """
+  기간 입력 안되었을 때 최소 일자, 최대 일자로 초기화 됨
+  code: 불러올 코인의 코드
+  interval: 불러올 거래 데이터 간격
+  start: 불러올 데이터의 시작 날짜 %Y-%m-%d %H:%M:%S
+  """
+  def __init__(self, intervals):
+    if intervals == 'all':
+      self.interval = ['1m', '3m', '5m', '10m', '30m', '1h', '6h', '12h', '24h' ]
+    else:
+      self.interval = intervals
+      
+  def _summary(self):
+    return f'Bithumb tickers {self.interval}'
+  
+  def _read(self, _conn):
     if start is None:
-      with self._conn.cursor() as curs:
+      with _conn.cursor() as curs:
         sql = f"SELECT min(date) FROM bithumb_{interval}_ohlcv WHERE code = '{code}'"
         curs.execute(sql)
         rs = curs.fetchone()
         start = datetime.strftime(rs[0], "%Y-%m-%d %H:%M:%S")
 
     if end is None:
-      with self._conn.cursor() as curs:
+      with _conn.cursor() as curs:
         sql = f"SELECT max(date) FROM bithumb_{interval}_ohlcv WHERE code = '{code}'"
         curs.execute(sql)
         rs = curs.fetchone()
@@ -46,13 +55,12 @@ class ohlcv_krw:
 
     #데이터를 받아올 쿼리문
     sql = f"SELECT * FROM bithumb_{interval}_ohlcv WHERE code = '{code}' and date >= '{start}' and date <= '{end}'"
-    df = pd.read_sql(sql, self._conn)
+    df = pd.read_sql(sql, _conn)
     df.index = df['date']
 
     #데이터 가공
     df = df[['code', 'open', 'high', 'low', 'close', 'volume']]
 
-    print(f'Loaded {interval} {code} Data from {df.index[0]} to {df.index[-1]}, {len(df)} Rows')
-    self._disconnectDB()
+    print(f'Bithumb Ohlcv_krw {interval} {code} from {df.index[0]} to {df.index[-1]}, {len(df)} Rows Loaded')
 
-    return df
+    return Ohlcv(df, title=f'Bithumb Ohlcv_krw {interval} {code}')
