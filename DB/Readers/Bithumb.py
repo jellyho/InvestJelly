32db8c93674124f82d2a5ded2394c6d54dd6fd04
jellyho <jellyho@naver.com>
2021-11-14 15:18:37 +0900

    Update Bithumb.py

--- DB/Readers/Bithumb.py
+++ DB/Readers/Bithumb.py
@@ -1,11 +1,14 @@
 import pandas as pd
 from ...Structures import TimeSeries, Ohlcv
+
 class tickers:
   def __init__(self, intervals):
     if intervals == 'all':
       self.interval = ['1m', '3m', '5m', '10m', '30m', '1h', '6h', '12h', '24h' ]
     else:
       self.interval = intervals
+  def _summary(self):
+    return f'Bithumb tickers {self.interval}'
   
   def _read(self, _conn):
     df = pd.DataFrame()
@@ -14,4 +17,3 @@ class tickers:
         sql = f"SELECT DISTINCT code FROM bithumb_{i}_ohlcv"
         df[i] = pd.read_sql(sql, self._conn)
     return TimeSeries(df)
-    
