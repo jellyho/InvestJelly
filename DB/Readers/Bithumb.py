db306389e358783e00f04c281daf76ca7f555f27
jellyho <jellyho@naver.com>
2021-11-14 15:16:32 +0900

    Update Bithumb.py

--- DB/Readers/Bithumb.py
+++ DB/Readers/Bithumb.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import ..Structures import TimeSeries, Ohlcv
+from ..Structures import TimeSeries, Ohlcv
 class tickers:
   def __init__(self, intervals):
     if intervals == 'all':
